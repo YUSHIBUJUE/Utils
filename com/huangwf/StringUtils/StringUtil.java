1db03640257cd9f42f4d6775317eebbcc5bb44f0
YUSHIBUJUE <YUSHIBUJUE@users.noreply.github.com>
2017-07-18 22:05:58 +0800

    Update StringUtil.java

--- com/huangwf/StringUtils/StringUtil.java
+++ com/huangwf/StringUtils/StringUtil.java
@@ -41,7 +41,34 @@ public class StringUtil {
 		
 		}
 	}
-  
+  	/**
+	 * @Function: parseMap
+	 * @Description: 将字符串解析成HashMap对象,其中KEY将统一使用大写
+	 *
+	 * @param paramString 要解析的字符串。多组键值之间用“|”分隔，键值之间用“^”分隔，如：key1^value1|key2^value2|key3^value3
+	 * @return
+	 * @throws Exception
+	 *
+	 * @version: v1.0.0
+	 * @author: huangwf
+	 *
+	 * Modification History:
+	 * Date         Author          Version            Description
+	 *---------------------------------------------------------*
+	 */
+	public static HashMap parseMap(String paramString) throws Exception {
+		HashMap arrayMap = new HashMap();
+		if (isNotBlank(paramString)) {
+			String[] conditionArray = paramString.split("\\|");
+			for (int i = 0; i < conditionArray.length; i++) {
+				int index = conditionArray[i].indexOf('^');
+				if (index > 1) {// 有分割符^并且不是在第一字符
+					arrayMap.put(conditionArray[i].substring(0, index).toUpperCase(), conditionArray[i].substring(index + 1));
+				}
+			}
+		}
+		return arrayMap;
+	}
    	public static boolean isBlank(String str){
            int strLen;
            if(str == null || (strLen =str.length())==0){
